f93a6b4ab548c753d105b14b6cf9e17886a52ef2
matt.huntsberry <matt.huntsberry@nielsen.com>
2021-10-17 14:42:55 -0700

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const gamesRouter = require("./routes/games");
 
